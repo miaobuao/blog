b938f1168670564f07e305449b75899de5273e08
miaobuao <miaobuao@outlook.com>
2023-07-25 17:02:26 +0800

    Tue Jul 25 05:02:26 PM CST 2023

--- submit.sh
+++ submit.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/fish
 git add .
-git commit -m "$(date)"
+
+read -l -p "echo 'commit: '" msg
+
+if test -n $(string trim $msg)
+    git commit -m $msg
+else
+    git commit -m "$(date)"
+end
+
 git push
