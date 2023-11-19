75d14f287edd0f30bd5e53ba89e35db85ef8e731
miaobuao <miaobuao@outlook.com>
2023-11-19 15:02:44 +0800

    auto generate sitemap

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,24 +1,28 @@
-import { getThemeConfig, defineConfig } from '@sugarat/theme/node'
+import { getThemeConfig, defineConfig } from "@sugarat/theme/node";
+import { createWriteStream } from "node:fs";
+import { resolve } from "node:path";
+import { SitemapStream } from "sitemap";
+import { PageData } from "vitepress";
+
+const links: { url: string; lastmod: PageData["lastUpdated"] }[] = [];
 
 const blogTheme = getThemeConfig({
   // 文章默认作者
-  author: '喵不嗷',
+  author: "喵不嗷",
   // 友链
   friend: [
     {
-      nickname: '喵不嗷',
-      des: 'TO BE A GEEK :)',
-      avatar:
-        '/avatar.png',
-      url: '/about'
+      nickname: "喵不嗷",
+      des: "TO BE A GEEK :)",
+      avatar: "/avatar.png",
+      url: "/about",
     },
-    {
-      nickname: 'IDIM',
-      des: '次元澡堂',
-      avatar:
-        'https://idim.cc/favicon.svg',
-      url: 'https://idim.cc'
-    },
+    // {
+    //   nickname: "IDIM",
+    //   des: "次元澡堂",
+    //   avatar: "https://idim.cc/favicon.svg",
+    //   url: "https://idim.cc",
+    // },
   ],
   comment: {
     repo: "miaobuao/miaobuao.github.io",
@@ -27,57 +31,65 @@ const blogTheme = getThemeConfig({
     categoryId: "DIC_kwDOJONNes4CVJhx",
     mapping: "title",
     // lang: 'zh-CN',
-    loading: 'lazy',
-    inputPosition: 'top',
+    loading: "lazy",
+    inputPosition: "top",
   },
   // 开启离线的全文搜索支持（如构建报错可注释下面的配置再次尝试）
-  search: 'pagefind',
-})
+  search: "pagefind",
+});
 
 export default defineConfig({
   markdown: {
     lineNumbers: true,
-    config: (md)=>{
-      md
-      .use(require("markdown-it-mathjax3"))
-      .use(require("markdown-it-footnote"))
-      // .use(require("markdown-it-implicit-figures"), {
-      //   figcaption: true,
-      //   lazyLoading: true
-      // })
-    }
-  }, 
+    config: (md) => {
+      md.use(require("markdown-it-mathjax3")).use(
+        require("markdown-it-footnote")
+      );
+    },
+  },
   ignoreDeadLinks: true,
   extends: blogTheme,
-  lang: 'zh-cmn-Hans',
-  title: 'Meo\'s Blog',
-  description: '喵不嗷的博客, miaobuao blog',
+  lang: "zh-cmn-Hans",
+  title: "Meo's Blog",
+  description: "喵不嗷的博客, miaobuao blog",
   vite: {
     optimizeDeps: {
-      include: ['element-plus'],
-      exclude: ['@sugarat/theme']
-    }
+      include: ["element-plus"],
+      exclude: ["@sugarat/theme"],
+    },
   },
   lastUpdated: true,
   themeConfig: {
-    lastUpdatedText: '上次更新于',
+    lastUpdatedText: "上次更新于",
     footer: {
       // message: '@miaobuao',9
       copyright:
-        '欢迎关注 | <a target="_blank" href="https://github.com/miaobuao"> @miaobuao </a>'
+        '欢迎关注 | <a target="_blank" href="https://github.com/miaobuao"> @miaobuao </a>',
     },
-    logo: 'https://avatars.githubusercontent.com/miaobuao',
-    // editLink: {
-    //   pattern:
-    //     'https://github.com/ATQQ/sugar-blog/tree/master/packages/blogpress/:path',
-    //   text: '去 GitHub 上编辑内容'
-    // },
-
+    logo: "https://avatars.githubusercontent.com/miaobuao",
     socialLinks: [
       {
-        icon: 'github',
-        link: 'https://github.com/miaobuao'
-      }
-    ]
-  }
-})
+        icon: "github",
+        link: "https://github.com/miaobuao",
+      },
+    ],
+  },
+  transformHtml: (_, id, { pageData }) => {
+    if (!/[\\/]404\.html$/.test(id))
+      links.push({
+        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, "$2"),
+        lastmod: pageData.lastUpdated,
+      });
+  },
+  buildEnd: async ({ outDir }) => {
+    // hostname 为线上域名
+    const sitemap = new SitemapStream({
+      hostname: "https://miaobuao.github.io/",
+    });
+    const writeStream = createWriteStream(resolve(outDir, "sitemap.xml"));
+    sitemap.pipe(writeStream);
+    links.forEach((link) => sitemap.write(link));
+    sitemap.end();
+    await new Promise((r) => writeStream.on("finish", r));
+  },
+});
